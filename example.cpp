97e41f17e05dca7f955a4ee3ad502cc385258868
hiamokkud <hiamokku@proton.me>
2025-12-14 16:04:17 +0100

    Added apple.

--- example.cpp
+++ example.cpp
@@ -3,9 +3,14 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
-struct BodyCell
+#include <random>
+struct Point
 {
     int x, y;
+    bool operator==(Point const &point)
+    {
+        return x == point.x && y == point.y;
+    }
 };
 enum Direction
 {
@@ -14,44 +19,57 @@ enum Direction
     Left,
     Right
 };
+
+Point getApplePoint(int gameFieldWidth, int gameFieldHeight, std::vector<Point> const &snake)
+{
+    static std::minstd_rand gen(
+        std::random_device{}());
+    std::uniform_int_distribution<int> widthDistr(0, gameFieldWidth-1), heightDistr(0, gameFieldHeight-1);
+    return Point{
+        widthDistr(gen), heightDistr(gen)};
+}
+
 int main(int argc, char **argv)
 {
     int gameFieldWidth = 20, gameFieldHeight = 20;
     int screenWidth = 800, screenHeight = 600;
 
-    std::vector<BodyCell> snake;
-    snake.push_back(BodyCell{gameFieldWidth / 2, gameFieldHeight / 2});
-    snake.push_back(BodyCell{gameFieldWidth / 2 - 1, gameFieldHeight / 2});
-    snake.push_back(BodyCell{gameFieldWidth / 2 - 2, gameFieldHeight / 2});
+    std::vector<Point> snake;
+    snake.push_back(Point{gameFieldWidth / 2, gameFieldHeight / 2});
+    snake.push_back(Point{gameFieldWidth / 2 - 1, gameFieldHeight / 2});
+    snake.push_back(Point{gameFieldWidth / 2 - 2, gameFieldHeight / 2});
 
-    Direction snakeDirection = Up;
+    Point apple = getApplePoint(gameFieldWidth, gameFieldHeight, snake);
+
+    Direction snakeDirection = Up, oldSnakeDirection = Up;
 
     InitWindow(screenWidth, screenHeight, "raylib [shapes] example - logo raylib");
+    SetTargetFPS(60);
 
     auto counter = std::chrono::steady_clock::now();
 
     while (!WindowShouldClose())
     {
         bool updateSnake = false;
-        if (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - counter) > std::chrono::milliseconds(2000))
+        if (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - counter) > std::chrono::milliseconds(200))
         {
             updateSnake = true;
             counter = std::chrono::steady_clock::now();
         }
 
-        if (IsKeyDown(KEY_A))
+        if (IsKeyDown(KEY_A) && oldSnakeDirection != Right)
             snakeDirection = Left;
-        else if (IsKeyDown(KEY_S))
+        else if (IsKeyDown(KEY_S) && oldSnakeDirection != Up)
             snakeDirection = Down;
-        else if (IsKeyDown(KEY_D))
+        else if (IsKeyDown(KEY_D) && oldSnakeDirection != Left)
             snakeDirection = Right;
-        else if (IsKeyDown(KEY_W))
+        else if (IsKeyDown(KEY_W) && oldSnakeDirection != Down)
             snakeDirection = Up;
 
         if (updateSnake)
         {
             { // snake movement
-                BodyCell nextPosition = snake[0];
+                Point nextPosition = snake[0];
                 std::cout << nextPosition.x << " " << nextPosition.y << std::endl;
                 if (snakeDirection == Left)
                     nextPosition.x = nextPosition.x == 0 ? gameFieldWidth - 1 : nextPosition.x - 1;
@@ -63,18 +81,25 @@ int main(int argc, char **argv)
                     nextPosition.y = nextPosition.y == 0 ? gameFieldHeight - 1 : nextPosition.y - 1;
                 for (int i = 0; i < snake.size(); i++)
                 {
-                    BodyCell currentPosition = snake[i];
+                    Point currentPosition = snake[i];
                     snake[i] = nextPosition;
                     nextPosition = currentPosition;
                 }
             }
-
-            ClearBackground(RAYWHITE);
-            for (int i = 0; i < snake.size(); i++)
+            if (snake[0] == apple)
             {
-                DrawRectangle(snake[i].x * screenWidth / gameFieldWidth, (gameFieldHeight - snake[i].y - 1) * screenHeight / gameFieldHeight, screenWidth / gameFieldWidth, screenHeight / gameFieldHeight, BLACK);
+                apple = getApplePoint(gameFieldWidth, gameFieldHeight, snake);
             }
+
+            oldSnakeDirection = snakeDirection;
         }
+
+        ClearBackground(RAYWHITE);
+        for (int i = 0; i < snake.size(); i++)
+        {
+            DrawRectangle(snake[i].x * screenWidth / gameFieldWidth, (gameFieldHeight - snake[i].y - 1) * screenHeight / gameFieldHeight, screenWidth / gameFieldWidth, screenHeight / gameFieldHeight, BLACK);
+        }
+        DrawRectangle(apple.x * screenWidth / gameFieldWidth, (gameFieldHeight - apple.y - 1) * screenHeight / gameFieldHeight, screenWidth / gameFieldWidth, screenHeight / gameFieldHeight, RED);
 
         EndDrawing();
     }
